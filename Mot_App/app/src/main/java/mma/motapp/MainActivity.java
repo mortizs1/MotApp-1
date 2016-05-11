997a64d3a032e78fa2ad93e449c520be5b75e411
michellpine <michellpine@hotmail.com>
2016-05-11 15:12:55 -0500

    Configuración del activity drawer

--- Mot_App/app/src/main/java/mma/motapp/MainActivity.java
+++ Mot_App/app/src/main/java/mma/motapp/MainActivity.java
@@ -1,9 +1,14 @@
 package mma.motapp;
 
+import android.Manifest;
+import android.app.FragmentManager;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,27 +21,42 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationView.OnNavigationItemSelectedListener, Favoritos.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {//, Favoritos.OnFragmentInteractionListener {
+
+    SupportMapFragment sMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        android.support.v4.app.FragmentManager sFm = getSupportFragmentManager();
+
+
+        sMapFragment = SupportMapFragment.newInstance();
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar,  R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        sMapFragment.getMapAsync(this);
+        sFm.beginTransaction().add(R.id.map, sMapFragment).commit();
     }
 
     @Override
@@ -79,18 +99,30 @@ public class MainActivity extends ActionBarActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        FragmentManager fm = getFragmentManager();
+        android.support.v4.app.FragmentManager sFm = getSupportFragmentManager();
+
         int id = item.getItemId();
 
-        if (id == R.id.nav_favoritos) {
+        if (sMapFragment.isAdded())
+            sFm.beginTransaction().hide(sMapFragment).commit();
+
+        if (id == R.id.nav_maps) {
+            getSupportActionBar().setTitle("Mapa");
+            if (!sMapFragment.isAdded())
+                sFm.beginTransaction().add(R.id.map, sMapFragment).commit();
+            else
+                sFm.beginTransaction().show(sMapFragment).commit();
+
+        } else if (id == R.id.nav_favoritos) {
+            getSupportActionBar().setTitle("Favoritos");
         } else if (id == R.id.nav_gallery) {
-            getSupportActionBar().setTitle("Prueba 0");
-
+            getSupportActionBar().setTitle("Galería");
         } else if (id == R.id.nav_slideshow) {
-            getSupportActionBar().setTitle("Prueba 1");
-
+            getSupportActionBar().setTitle("Slider");
+            fm.beginTransaction().replace(R.id.content_frame, new ImportFragment()).commit();
         } else if (id == R.id.nav_config) {
-
+            getSupportActionBar().setTitle("Configuración");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -99,7 +131,60 @@ public class MainActivity extends ActionBarActivity
     }
 
     @Override
-    public void onFragmentInteraction(Uri uri) {
-    }
+    public void onMapReady(GoogleMap mMap) {
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMap().setPadding(0, 1400, 0, 0);
+
+        LatLng medellin = new LatLng(6.244203, -75.58121189999997);
+        LatLng penthouse = new LatLng(6.2582513, -75.57997879999999);
+        LatLng thematic = new LatLng(6.2475828, -75.56012269999997);
+        LatLng pegasus = new LatLng(6.2767208, -75.6189794);
+        LatLng collins = new LatLng(6.2665204, -75.602798);
+        LatLng dlusso = new LatLng(6.293998500000001, -75.57108920000002);
+        LatLng momentos = new LatLng(6.2469813, -75.56813360000001);
+        LatLng eros = new LatLng(6.2546754, -75.5703656);
+        LatLng metropolis = new LatLng(6.2673888, -75.562344);
+        LatLng ejecutivo = new LatLng(6.2387662, -75.59996990000002);
+        LatLng puntozero = new LatLng(6.2582513, -75.57997879999999);
+
+        mMap.addMarker(new MarkerOptions().position(penthouse).title("Motel Penthouse")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(thematic).title("Thematic Suites")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(pegasus).title("Motel Pegasus")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(collins).title("Motel Collins")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(dlusso).title("D'Lusso")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(momentos).title("Momentos Suites")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(eros).title("Motel Eros")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(metropolis).title("Motel Metrópolis")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(ejecutivo).title("Motel Ejecutivo La 33")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mMap.addMarker(new MarkerOptions().position(puntozero).title("Punto Zero")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(medellin, 12));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+    }
 }
